709f6722123f6d800be06682771d9bdf6a97fffc
syounng <stellaj0522@gmail.com>
2025-09-20 15:05:30 +0900

    feat(Screening): 상영 스케줄 유효성 검증
    여러 스케줄 중에서 겹치는 스케줄이 하나라도 있으면 예외가 발생한다.

---
+++ src/main/java/domain/Screening.java
@@ -0,0 +1,25 @@
+package domain;
+
+import java.util.List;
+
+public class Screening {
+    private final List<Schedule> scheduleList;
+
+    public Screening(List<Schedule> scheduleList) {
+        validateSchedules(scheduleList);
+        this.scheduleList = scheduleList;
+    }
+
+    private void validateSchedules(List<Schedule> scheduleList) {
+        for (int i = 0; i < scheduleList.size(); i++) {
+            for (int j = i + 1; j < scheduleList.size(); j++) {
+                Schedule s1 = scheduleList.get(i);
+                Schedule s2 = scheduleList.get(j);
+                if (!s1.isValidSchedule(s2)) {
+                    throw new IllegalArgumentException("스케줄이 서로 겹칩니다: " + s1 + " vs " + s2);
+                }
+            }
+        }
+    }
+
+}
---
+++ src/test/java/domain/ScreeningTest.java
@@ -0,0 +1,58 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+class ScreeningTest {
+
+    @Test
+    void 영화_상영의_스케줄_중에_겹치는_스케줄이_있다면_예외를_던진다() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Screening(
+                    List.of(
+                            new Schedule(
+                                    LocalTime.of(1, 0),
+                                    LocalTime.of(3, 0)
+                            ),
+                            new Schedule(
+                                    LocalTime.of(1, 0),
+                                    LocalTime.of(2, 0)
+                            ),
+                            new Schedule(
+                                    LocalTime.of(1, 0),
+                                    LocalTime.of(1, 0)
+                            ),
+                            new Schedule(
+                                    LocalTime.of(1, 0),
+                                    LocalTime.of(1, 0)
+                            )
+                    )
+            );
+        });
+    }
+
+    @Test
+    void 영화_상영의_스케줄_중에_겹치는_스케줄이_없다면_예외를_던지지_않는다() {
+        assertThatNoException().isThrownBy(() -> {
+            new Screening(
+                    List.of(
+                            new Schedule(
+                                    LocalTime.of(1, 0),
+                                    LocalTime.of(3, 0)
+                            ),
+                            new Schedule(
+                                    LocalTime.of(3, 1),
+                                    LocalTime.of(4, 0)
+                            )
+                    )
+            );
+        });
+    }
+
+
+}
